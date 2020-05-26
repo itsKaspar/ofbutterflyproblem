4d116ee9e5a1e31a8f7dd325febe7559b66e5456
Kaspar RAVEL <kaspar.ravel@gmail.com>
2020-05-26 05:24:36 +0200

    Update Boid.cpp

--- Boid.cpp
+++ Boid.cpp
@@ -1,30 +1,63 @@
-#ifndef _BOID
-#define _BOID
+#include "Boid.h"
 
-#include "ofMain.h" 
+Boid::Boid() {
+}
+Boid::~Boid() {
+}
 
-#pragma once
-class Boid
-{
+Boid::Boid(glm::vec3 p) {
 
-public:
+	vel = glm::vec3(1, 0,0);
+	pos = p;
+	butterfly.move(p);
 
-	Boid();
-	~Boid();
-	Boid(glm::vec3 p);
-	void update();
-	void draw();
+	glm::vec3 wingSize(100, 100, 0);
 
-	glm::vec3 pos;
-	glm::vec3 vel;
+	lWing.setParent(butterfly);
+	rWing.setParent(butterfly);
 
-	ofNode butterfly;
-	ofPlanePrimitive lWing;
-	ofPlanePrimitive rWing;
+	lWing.set(wingSize.x, wingSize.y, 2, 2);
+	rWing.set(wingSize.x, wingSize.y, 2, 2);
 
-	ofEasyCam cam;
+	glm::vec3 rWingOffset(100, 100, 0);
 
-private:
-};
+	//move all the vertices so these start at 0,0
+	for (auto&v : rWing.getMesh().getVertices())
+	{
+		v += rWingOffset / 2.0;
+	}
 
-#endif
+	glm::vec3 lrWingOffset(100, -100, 0);
+
+	//move all the vertices so these start at 0,0
+	for (auto&v : lWing.getMesh().getVertices())
+	{
+		v += lrWingOffset / 2.0;
+	}
+
+}
+
+void Boid::update() {
+	pos += vel;
+	butterfly.move(vel);
+	butterfly.lookAt(butterfly.getGlobalPosition() + vel);
+
+	auto rWingOffset = glm::vec3(0, 60, 0);
+
+	lWing.setOrientation(glm::angleAxis(ofMap(sin(ofGetElapsedTimef()), -1, 1, -PI * 0.25, PI*0.25), glm::vec3(1, 0, 0)));
+	rWing.setOrientation(glm::angleAxis(ofMap(sin(ofGetElapsedTimef()), -1, 1, PI * 0.25, -PI*0.25), glm::vec3(1, 0, 0)));
+}
+
+void Boid::draw() {
+
+	cam.begin();
+
+	ofDrawCircle(500, 100, 0, 5);
+
+	butterfly.draw();
+
+	rWing.draw();
+	lWing.draw();
+
+	cam.end();
+}
